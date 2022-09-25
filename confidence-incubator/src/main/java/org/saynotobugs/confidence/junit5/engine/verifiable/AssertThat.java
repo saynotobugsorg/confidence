855d36a680d7f573818776bb6bd76c027d8aee5d
Marten Gajda <marten@dmfs.org>
2022-09-25 14:49:27 +0200

    Introduce Test Expression Engine, closes #59
    This adds an experimental TestEngine to run purely declarative tests.

---
+++ confidence-incubator/src/main/java/org/saynotobugs/confidence/junit5/engine/verifiable/AssertThat.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.saynotobugs.confidence.junit5.engine.verifiable;
+
+import org.dmfs.srcless.annotations.staticfactory.StaticFactories;
+import org.saynotobugs.confidence.Quality;
+import org.saynotobugs.confidence.junit5.engine.Verifiable;
+
+import static org.saynotobugs.confidence.Assertion.assertThat;
+
+
+@StaticFactories(value = "ConfidenceEngine", packageName = "org.saynotobugs.confidence.junit5.engine")
+public final class AssertThat<T> implements Verifiable
+{
+    private final String mName;
+    private final T mInstance;
+    private final Quality<T> mQuality;
+
+
+    public AssertThat(String name, T instance, Quality<T> quality)
+    {
+        mName = name;
+        mInstance = instance;
+        mQuality = quality;
+    }
+
+
+    public AssertThat(T instance, Quality<T> quality)
+    {
+        this("", instance, quality);
+    }
+
+
+    @Override
+    public void verify()
+    {
+        assertThat(mInstance, mQuality);
+    }
+
+
+    @Override
+    public String name()
+    {
+        return mName;
+    }
+
+}
---
+++ confidence-incubator/src/test/java/org/saynotobugs/confidence/junit5/engine/testengine/ConfidenceEngineTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2023 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.saynotobugs.confidence.junit5.engine.testengine;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.testkit.engine.EngineTestKit;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+
+class ConfidenceEngineTest
+{
+    @Test
+    void testAllPass()
+    {
+        EngineTestKit
+            .engine("ConfidenceTest")
+            .selectors(selectClass(AllVerifiablePassTestCase.class))
+            .execute()
+            .testEvents()
+            .assertStatistics(stats -> stats
+                .started(3)
+                .succeeded(3)
+                .failed(0)
+            );
+    }
+
+
+    @Test
+    void testMultipleFailPass()
+    {
+        EngineTestKit
+            .engine("ConfidenceTest")
+            .selectors(selectClass(MultipleVerifiableFailAndPassTestCase.class))
+            .execute()
+            .testEvents()
+            .assertStatistics(stats -> stats
+                .started(3)
+                .succeeded(1)
+                .aborted(0)
+                .failed(2)
+            );
+    }
+
+
+    @Test
+    void testOnePass()
+    {
+        EngineTestKit
+            .engine("ConfidenceTest")
+            .selectors(selectClass(SingleVerifiablePassTestCase.class))
+            .execute()
+            .testEvents()
+            .assertStatistics(stats -> stats
+                .started(1)
+                .succeeded(1)
+                .failed(0)
+            );
+    }
+
+
+    @Test
+    void testOneFail()
+    {
+        EngineTestKit
+            .engine("ConfidenceTest")
+            .selectors(selectClass(SingleVerifiableFailTestCase.class))
+            .execute()
+            .testEvents()
+            .assertStatistics(stats -> stats
+                .started(1)
+                .succeeded(0)
+                .failed(1)
+            );
+    }
+}
