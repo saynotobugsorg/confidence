6937475862cee234cb77046bb7f1378cc3568861
Marten Gajda <marten@dmfs.org>
2023-03-18 02:19:13 +0100

    Provide SystemOut resource, closes #64

---
+++ confidence-incubator/src/test/java/org/saynotobugs/confidence/junit5/engine/resource/SystemOutTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.saynotobugs.confidence.junit5.engine.resource;
+
+import org.dmfs.jems2.Generator;
+import org.junit.jupiter.api.Test;
+import org.saynotobugs.confidence.description.Text;
+import org.saynotobugs.confidence.junit5.engine.quality.ResourceThat;
+
+import static org.dmfs.jems2.confidence.Jems2.hasValue;
+import static org.saynotobugs.confidence.Assertion.assertThat;
+import static org.saynotobugs.confidence.quality.Core.*;
+
+
+class SystemOutTest
+{
+    @Test
+    void test()
+    {
+        assertThat(new SystemOut(),
+            hasValue(new ResourceThat<>(
+                allOf(
+                    has("generated", Generator::next, emptyCharSequence()),
+                    successfully(new Text("Writes to System.out"), new Text("Writes to System.out"),
+                        (Generator<?> systemOut) -> System.out.print("Hello Test")),
+                    has("generated", Generator::next, equalTo("Hello Test"))
+                ),
+                allOf(
+                    has("generated", Generator::next, equalTo("Hello Test")),
+                    successfully(new Text("Writes to System.out"), new Text("Writes to System.out"),
+                        (Generator<?> systemOut) -> System.out.print("More Output")),
+                    // no further changes expected
+                    has("generated", Generator::next, equalTo("Hello Test"))
+                ))));
+    }
+
+}
