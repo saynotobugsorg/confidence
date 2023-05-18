2d2e51c4a0cc81e1d0e01b8479162f9f8496cd5c
Marten Gajda <marten@dmfs.org>
2023-05-18 16:48:07 +0200

    MutatedBy Quality, closes #74
    Adds a Quality to test mutation

---
+++ confidence-core/src/main/java/org/saynotobugs/confidence/quality/object/MutatedBy.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.saynotobugs.confidence.quality.object;
+
+import org.dmfs.jems2.Generator;
+import org.dmfs.jems2.Procedure;
+import org.dmfs.srcless.annotations.staticfactory.StaticFactories;
+import org.saynotobugs.confidence.Assessment;
+import org.saynotobugs.confidence.Description;
+import org.saynotobugs.confidence.Quality;
+import org.saynotobugs.confidence.assessment.FailPrepended;
+import org.saynotobugs.confidence.description.Spaced;
+import org.saynotobugs.confidence.description.TextDescription;
+
+
+@StaticFactories(value = "Core", packageName = "org.saynotobugs.confidence.quality")
+public final class MutatedBy<T> implements Quality<Generator<T>>
+{
+    private final Description mValueDescription;
+    private final Procedure<? super T> mMutator;
+    private final Quality<? super T> mValueQuality;
+
+
+    public MutatedBy(Description valueDescription, Procedure<? super T> mutator, Quality<? super T> valueQuality)
+    {
+        mValueDescription = valueDescription;
+        mMutator = mutator;
+        mValueQuality = valueQuality;
+    }
+
+
+    @Override
+    public Assessment assessmentOf(Generator<T> candidate)
+    {
+        T value = candidate.next();
+        mMutator.process(value);
+        return new FailPrepended(new Spaced(new TextDescription("mutated by"), mValueDescription),
+            mValueQuality.assessmentOf(value));
+    }
+
+
+    @Override
+    public Description description()
+    {
+        return new Spaced(new TextDescription("mutated by"), mValueDescription, mValueQuality.description());
+    }
+}
