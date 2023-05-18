32d35f7294c1fd28252b54eedb4d76ce641b9eeb
Marten Gajda <marten@dmfs.org>
2023-05-18 17:02:52 +0200

    MutatedBy Quality, closes #74 (#80)
    Adds a Quality to test mutation

---
+++ confidence-core/src/test/java/org/saynotobugs/confidence/quality/object/MutatedByTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.saynotobugs.confidence.quality.object;
+
+import org.junit.jupiter.api.Test;
+import org.saynotobugs.confidence.description.TextDescription;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.saynotobugs.confidence.Assertion.assertThat;
+import static org.saynotobugs.confidence.quality.Core.allOf;
+import static org.saynotobugs.confidence.quality.Core.iterates;
+import static org.saynotobugs.confidence.test.quality.Test.*;
+
+
+class MutatedByTest
+{
+    @Test
+    void test()
+    {
+        assertThat(new MutatedBy<List<String>>(new TextDescription("appending abc"), l -> l.add("abc"), iterates("abc")),
+            allOf(
+                passes(ArrayList::new, LinkedList::new),
+                fails(() -> new ArrayList<>(asList("123")), "mutated by appending abc iterated [ 0: \"123\",\n  1: additional \"abc\" ]"),
+                hasDescription("mutated by appending abc iterates [ 0: \"abc\" ]")
+            )
+        );
+    }
+
+}
