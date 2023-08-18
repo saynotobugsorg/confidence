5bcaf3ed5c99584f02ca5fad6cb01eef667d7ba5
Marten Gajda <marten@dmfs.org>
2023-08-18 23:53:47 +0200

    Add ContainsPattern Quality, closes #72
    This is just like MatchesPattern but uses `find`

---
+++ confidence-core/src/test/java/org/saynotobugs/confidence/quality/charsequence/ContainsPatternTest.java
@@ -0,0 +1,25 @@
+package org.saynotobugs.confidence.quality.charsequence;
+
+import org.junit.jupiter.api.Test;
+import org.saynotobugs.confidence.quality.composite.AllOf;
+import org.saynotobugs.confidence.test.quality.Fails;
+import org.saynotobugs.confidence.test.quality.HasDescription;
+import org.saynotobugs.confidence.test.quality.Passes;
+
+import static org.saynotobugs.confidence.Assertion.assertThat;
+
+
+class ContainsPatternTest
+{
+    @Test
+    void test()
+    {
+        assertThat(new ContainsPattern("123"),
+            new AllOf<>(
+                new Passes<>("123", "abc123xyz"),
+                new Fails<>("12", "\"12\" did not contain pattern /123/"),
+                new Fails<>("abc", "\"abc\" did not contain pattern /123/"),
+                new HasDescription("contains pattern /123/")
+            ));
+    }
+}
