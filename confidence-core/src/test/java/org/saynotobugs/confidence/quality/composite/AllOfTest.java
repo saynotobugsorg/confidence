f2825b44e3cd86221eaedb98b931c8250178e2d1
Marten Gajda <marten@dmfs.org>
2023-10-04 13:45:28 +0200

    Fix AllOf being lazy, fixes #115 (#116)

--- confidence-core/src/test/java/org/saynotobugs/confidence/quality/composite/AllOfTest.java
+++ confidence-core/src/test/java/org/saynotobugs/confidence/quality/composite/AllOfTest.java
@@ -1,11 +1,18 @@
 package org.saynotobugs.confidence.quality.composite;
 
 import org.junit.jupiter.api.Test;
+import org.saynotobugs.confidence.description.Text;
+import org.saynotobugs.confidence.quality.iterable.Contains;
 import org.saynotobugs.confidence.quality.trivial.Anything;
 import org.saynotobugs.confidence.quality.trivial.Nothing;
 import org.saynotobugs.confidence.test.quality.Fails;
 import org.saynotobugs.confidence.test.quality.Passes;
+import org.saynotobugs.confidence.test.quality.PassesPostMutation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.saynotobugs.confidence.Assertion.assertThat;
 
 
@@ -26,5 +33,13 @@ class AllOfTest
         assertThat(new AllOf<>(new Nothing()), new Fails<>(123, "{ <123> }"));
         assertThat(new AllOf<>(new Anything(), new Nothing()), new Fails<>(123, "{ ...\n  <123> }"));
         assertThat(new AllOf<>(new Nothing(), new Anything(), new Anything()), new Fails<>(123, "{ <123>\n  ... }"));
+    }
+
+
+    @Test
+    void testPostMutation()
+    {
+        assertThat(new AllOf<>(new Contains<>(1), new Contains<>(2)),
+            new PassesPostMutation<>(() -> new ArrayList<>(asList(1, 2)), new Text("clear list"), List::clear));
     }
 }
